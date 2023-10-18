099c7a185c79a6672cdbd9a3334c1b6075bd808a
Babs-sammy <babalolasamuel2006@gmail.com>
2023-10-18 12:51:30 +0200

    resume

--- resume.js
+++ resume.js
@@ -1,14 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var sidemenu = document.getElementById("sidemenu");
-
+var sidemenu = document.getElementById("sidemenu");
 function openmenu(){
     sidemenu.style.right = "0";
 }
-
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
-});
-
-
-
